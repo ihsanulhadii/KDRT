37cddcee67cc60cc74d96e3043cbbfb1c16f5e0f
asywalulfikri <asywalulfikri@yahoo.co.id>
2023-08-18 10:35:50 +0700

    commit

--- app/src/main/java/com/example/myapplication/activity/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication/activity/ActivityLogin.java
@@ -2,6 +2,7 @@ package com.example.myapplication.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,10 +10,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.myapplication.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class ActivityLogin extends AppCompatActivity {
  TextView tvCreateAccount;
  AppCompatButton btnLogin;
+
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static final String TAG = "ActivityLogin";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,20 @@ public class ActivityLogin extends AppCompatActivity {
 
         tvCreateAccount = findViewById(R.id.tvCreateAccount);
         btnLogin = findViewById(R.id.btnLogin);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
 
         tvCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,4 +66,32 @@ public class ActivityLogin extends AppCompatActivity {
             }
         });
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            Log.d(TAG, "signInResult:success");
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failure, code=" + e.getStatusCode());
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/activity/ActivityRegister.java
+++ app/src/main/java/com/example/myapplication/activity/ActivityRegister.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,21 @@ import com.example.myapplication.R;
 
 public class ActivityRegister extends AppCompatActivity {
 
+    TextView tvAlready;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvAlready = findViewById(R.id.tvAlready);
+
+        tvAlready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pagelogin = new Intent(ActivityRegister.this,ActivityLogin.class);
+                startActivity(pagelogin);
+            }
+        });
     }
 }
