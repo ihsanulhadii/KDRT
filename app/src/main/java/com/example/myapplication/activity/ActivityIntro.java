4ffde0ed0c05c2b34ece7d9b73164b217e1da85b
asywalulfikri <asywalulfikri@yahoo.co.id>
2023-10-10 13:16:56 +0700

    Membuat buttonn untuk website di page intro

--- app/src/main/java/com/example/myapplication/activity/ActivityIntro.java
+++ app/src/main/java/com/example/myapplication/activity/ActivityIntro.java
@@ -3,16 +3,21 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
 public class ActivityIntro extends AppCompatActivity {
 
     AppCompatButton btnGet;
+
+    Button btnOpenWebsite;
 
     SharedPreferences sharedPreferences;
 
@@ -44,5 +49,20 @@ public class ActivityIntro extends AppCompatActivity {
         }
 
 
+    }
+    public void openWebsite(View view) {
+        // URL website yang ingin diarahkan
+        String websiteUrl = "https://kawancerita.id/";
+
+        // Buat Intent untuk membuka browser dengan URL website
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl));
+
+        // Coba untuk menjalankan Intent
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // Handle kesalahan jika tidak ada aplikasi browser yang tersedia
+            e.printStackTrace();
+        }
     }
 }
